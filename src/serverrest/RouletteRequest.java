8a6d590def8810128a217dda03227ae8586da993
chiasserinijacopo <chiasserini.jacopo@franchettisalviani.net>
2026-02-20 09:35:59 +0100

    implementata classe RouletteRequest

---
+++ src/serverrest/RouletteRequest.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serverrest;
+
+/**
+ *
+ * @author delfo
+ */
+public class RouletteRequest {
+    public String giocata;
+    public Integer numero;  
+    
+    // Costruttore vuoto necessario per GSON
+    public RouletteRequest() {
+    }
+    
+    public RouletteRequest(String giocata, Integer numero) {  
+        this.giocata = giocata;
+        this.numero = numero;
+    }
+
+
+    public String getGiocata() {
+        return giocata;
+    }
+
+    public void setGiocata(String giocata) {
+        this.giocata = giocata;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "RouletteRequest{" + "giocata=" + giocata + ", numero=" + numero + '\'' +'}';
+    }
+    
+}
